4f3e107b4805609670e2ec999d1812d98c738e52
huguodong <531035580@QQ.COM>
2016-01-06 10:43:52 +0800

    无

---
+++ WaveLoading/obj/Debug/generated/src/__NamespaceMapping__.cs
@@ -0,0 +1,2 @@
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "me.itangqi.library", Managed="ME.Itangqi.Library")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "me.itangqi.waveloadingview", Managed="ME.Itangqi.Waveloadingview")]
